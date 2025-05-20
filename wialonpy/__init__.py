dc98fa3ad00dc25bf0b931b9d1f03b4a05417a53
ss <shambazov@gmail.com>
2025-05-20 13:18:29 +0200

    refactor: update __init__.py to include get_session_eid and get_wialon_units chore: bump version to 0.1.6

--- wialonpy/__init__.py
+++ wialonpy/__init__.py
@@ -1,3 +1,6 @@
 from .utils import get_session_eid
 
-__all__ = ["get_session_eid"]
+__all__ = ["get_session_eid", "get_wialon_object_id",
+            "get_wialon_report_id","wialon_exec_report",
+            "wialon_select_result","get_wialon_units",
+            ]
